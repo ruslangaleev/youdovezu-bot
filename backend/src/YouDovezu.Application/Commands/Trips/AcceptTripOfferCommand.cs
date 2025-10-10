a1316141637e8d83837b54a95db7653cc0ae1ce2
galeev ruslan <ruslan.nafisovich@gmail.com>
2025-10-10 17:02:58 +0500

    delete project because don't run project with debug

--- backend/src/YouDovezu.Application/Commands/Trips/AcceptTripOfferCommand.cs
+++
@@ -1,9 +0,0 @@
-using YouDovezu.Application.Common.Models;
-
-namespace YouDovezu.Application.Commands.Trips;
-
-public class AcceptTripOfferCommand : BaseCommand<bool>
-{
-    public Guid TripRequestId { get; set; }
-    public Guid OfferId { get; set; }
-}
